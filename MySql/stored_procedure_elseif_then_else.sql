6260f6e899c48dc6b11004a3e4e3571f5f318d7d
Ially Fabiane da Silva <94318317+IallyFabiane@users.noreply.github.com>
2023-07-20 15:00:18 -0300

    Update stored_procedure_elseif_then_else.sql

--- MySql/stored_procedure_elseif_then_else.sql
+++ MySql/stored_procedure_elseif_then_else.sql
@@ -2,7 +2,7 @@ CREATE DEFINER=`root`@`localhost` PROCEDURE `Preco_Produto`(codigo VARCHAR(50))
 BEGIN
 	DECLARE preco FLOAT;
     DECLARE mensagem VARCHAR(30);
-    SELECT PRECO_LISTA INTO preco FROM produtos WHERE CODIGO = codigo;
+    SELECT PRECO_LISTA INTO preco FROM produtos WHERE CODIGO = codigo LIMIT 1;
 	IF preco >= 12 THEN
       SET mensagem = 'produto caro';
 	ELSEIF preco <= 7 AND preco < 12 THEN
@@ -10,4 +10,5 @@ BEGIN
 	ELSE
 	   SET mensagem = 'produto barato';
 	END IF;
+    SELECT mensagem;
 END
