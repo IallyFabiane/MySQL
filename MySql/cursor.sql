4476099f265f75e407a2dcedae53e9b7b221f033
Ially Fabiane da Silva <94318317+IallyFabiane@users.noreply.github.com>
2023-07-24 15:00:33 -0300

    Add files via upload

---
+++ MySql/cursor.sql
@@ -0,0 +1,17 @@
+CREATE DEFINER=`root`@`localhost` PROCEDURE `Cursor_Looping`()
+BEGIN
+    DECLARE fimDoCursor int DEFAULT 0;
+	DECLARE nome varchar(50);
+    DECLARE c CURSOR FOR 
+    SELECT NOME FROM clientes LIMIT 4;
+    DECLARE CONTINUE HANDLER FOR NOT FOUND SET fimDoCursor = 1;
+    OPEN c;
+    WHILE fimDoCursor = 0
+    DO 
+		FETCH c INTO nome;
+		IF fimDoCursor  = 0 THEN
+			SELECT nome;
+		END IF;
+    END WHILE;
+    CLOSE c;
+END
